b938de87d3ff0fc9e0af884d39d043dea59a7f4e
Shanshan <shanshangilbert@gmail.com>
2024-09-11 14:19:53 +0900

    Update and rename part15-findridelength.sql to part15-findrideduration.sql

--- usedSQLcodes/database/part15-findrideduration.sql
+++ usedSQLcodes/database/part15-findrideduration.sql
@@ -1,15 +1,15 @@
--- To add ride length column to table, and then add data
+-- To add ride duration column to table, and then add data
 -- I did not change SQL_SAFE_UPDATES every time, but left it as a reminder to do so.
 -- Checked each time to make sure the new data was inputted accordingly
 
 -- For Jan
 ALTER TABLE cyclistic.`202301-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202301-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -18,12 +18,12 @@ FROM cyclistic.`202301-tripdata`
 
 -- For Feb
 ALTER TABLE cyclistic.`202302-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202302-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -32,12 +32,12 @@ FROM cyclistic.`202302-tripdata`
 
 -- For Mar
 ALTER TABLE cyclistic.`202303-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202303-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -46,12 +46,12 @@ FROM cyclistic.`202303-tripdata`
 
 -- For Apr
 ALTER TABLE cyclistic.`202304-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202304-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -60,12 +60,12 @@ FROM cyclistic.`202304-tripdata`
 
 -- For May
 ALTER TABLE cyclistic.`202305-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202305-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -74,12 +74,12 @@ FROM cyclistic.`202305-tripdata`
 
 -- For Jun
 ALTER TABLE cyclistic.`202306-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202306-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -88,12 +88,12 @@ FROM cyclistic.`202306-tripdata`
 
 -- For Jul
 ALTER TABLE cyclistic.`202307-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202307-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -102,12 +102,12 @@ FROM cyclistic.`202307-tripdata`
 
 -- For Aug
 ALTER TABLE cyclistic.`202308-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202308-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -116,12 +116,12 @@ FROM cyclistic.`202308-tripdata`
 
 -- For Sep
 ALTER TABLE cyclistic.`202309-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202309-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -130,12 +130,12 @@ FROM cyclistic.`202309-tripdata`
 
 -- For Oct
 ALTER TABLE cyclistic.`202310-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202310-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -144,12 +144,12 @@ FROM cyclistic.`202310-tripdata`
 
 -- For Nov
 ALTER TABLE cyclistic.`202311-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202311-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
@@ -158,12 +158,12 @@ FROM cyclistic.`202311-tripdata`
 
 -- For Dec
 ALTER TABLE cyclistic.`202312-tripdata`
-ADD COLUMN ride_length INT DEFAULT NULL
+ADD COLUMN ride_duration INT DEFAULT NULL
 
 SET SQL_SAFE_UPDATES = 0
 
 UPDATE cyclistic.`202312-tripdata`
-SET ride_length = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
+SET ride_duration = TIMESTAMPDIFF(MINUTE, started_at, ended_at)
 
 SET SQL_SAFE_UPDATES = 1 
 
