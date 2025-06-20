cee53541c809a4d4da32eb1a7790afd57dc97eeb
Shanshan <shanshangilbert@gmail.com>
2025-06-20 16:45:53 +0900

    Add files via upload

---
+++ usedSQLcodes/analysis/memberanalysis.sql
@@ -0,0 +1,107 @@
+-- For members
+
+SELECT
+*
+FROM
+cyclistic.`members`
+
+-- Per month
+SELECT
+month(started_at) AS month,
+COUNT(ride_id) AS number_of_rides,
+AVG(ride_duration) AS avg_ride_duration
+FROM
+cyclistic.`members`
+GROUP BY
+month
+ORDER BY
+1
+
+-- Average number of rides per month
+SELECT
+AVG(number_of_rides) AS avg_num_of_rides
+FROM (
+SELECT
+month(started_at) AS month,
+COUNT(ride_id) AS number_of_rides
+FROM
+cyclistic.`members`
+GROUP BY
+month
+ORDER BY
+1
+) AS average_for_months
+
+-- Day of week, total rides and average ride duration per day
+SELECT
+day_of_week,
+COUNT(ride_id) AS total_rides_per_day,
+AVG(ride_duration) AS avg_ride_duration
+FROM
+cyclistic.`members`
+GROUP BY
+day_of_week
+ORDER BY
+FIELD(day_of_week, 'SUNDAY', 'MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY', 'SATURDAY')
+
+-- Bike type
+SELECT
+rideable_type,
+COUNT(ride_id)
+FROM
+cyclistic.`members`
+GROUP BY
+rideable_type
+ORDER BY
+2 desc
+
+-- Popular start station
+SELECT
+start_station_id,
+station_name,
+COUNT(ride_id)
+FROM
+cyclistic.`members` m JOIN
+cyclistic.`stations` s 
+on m.start_station_id = s.station_id
+GROUP BY
+start_station_id
+ORDER BY
+3 desc
+LIMIT 5
+
+
+-- Popular end station
+SELECT
+end_station_id,
+station_name,
+COUNT(ride_id)
+FROM
+cyclistic.`members` m JOIN
+cyclistic.`stations` s 
+on m.end_station_id = s.station_id
+GROUP BY
+end_station_id
+ORDER BY
+3 desc
+LIMIT 5
+
+
+
+-- Longest ride (shortest rides are all 1 min)
+SELECT
+ride_id,
+started_at,
+start_station_id,
+ended_at,
+end_station_id,
+end_lat,
+end_lng,
+ride_duration
+FROM
+cyclistic.`members`
+GROUP BY
+ride_id
+ORDER BY
+8 desc
+LIMIT 5
